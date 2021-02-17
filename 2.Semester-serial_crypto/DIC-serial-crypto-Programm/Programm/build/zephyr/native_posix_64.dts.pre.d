1357d2507fd96986025769bb5072b419546c9616
Fabio Plunser <f.plunser@outlook.com>
2021-02-17 13:22:45 +0100

    update

---
+++ 2.Semester-serial_crypto/DIC-serial-crypto-Programm/Programm/build/zephyr/native_posix_64.dts.pre.d
@@ -0,0 +1,8 @@
+empty_file.o: /home/peppi/zephyrproject/zephyr/misc/empty_file.c \
+ /home/peppi/zephyrproject/zephyr/boards/posix/native_posix/native_posix_64.dts \
+ /home/peppi/zephyrproject/zephyr/boards/posix/native_posix/native_posix.dts \
+ /home/peppi/zephyrproject/zephyr/dts/posix/posix.dtsi \
+ /home/peppi/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/peppi/zephyrproject/zephyr/dts/common/mem.h \
+ /home/peppi/zephyrproject/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/peppi/zephyrproject/zephyr/include/dt-bindings/gpio/gpio.h
